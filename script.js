ac76bb2fa0ff4a0ca442d831aadbfa6200de190f
mustafasoft <mustafat@softinnovas.com>
2022-03-30 18:02:32 +0300

    fix bmi calculation formula

---
+++ script.js
@@ -0,0 +1,30 @@
+
+let firstBill = 124;
+let secondBill = 48;
+let thirdBill = 75;
+let fourthBill = 268;
+let totalBudget = 750;
+
+ 
+function calculateTips (bill) {
+    let tip = bill * 0.1;
+    
+    
+    
+    
+    
+    return tip;
+}
+
+let tip_1 = calculateTips(firstBill);  // ==> 12,4
+let tip_2 = calculateTips(fourthBill);  // ==> 26,8
+
+console.log(tip_1)
+console.log(tip_2)
+
+// getTotalTips 
+
+
+
+
+// budgetDust
